b15cee5db23ac63e541ad4fe83f674668f6c7ec3
Ali anjo <alianjo178@gmail.com>
2025-07-26 18:02:17 +0000

    Add base examples

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "ami_id" {
+    description = "Amazon machine Image ID"
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+
+variable "key_name" {
+    description = "SSH key pair name"
+  
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the instance will be deployed"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for EC2"
+}
+
+variable "ssh_cidr" {
+  default     = "0.0.0.0/0"
+  description = "CIDR blocks allowed to SSH"
+}
