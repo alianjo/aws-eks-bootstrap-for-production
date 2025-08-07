23dd95cc047cd6168d43f2728a32d81eb3fa2017
Ali anjo <alianjo178@gmail.com>
2025-08-07 05:27:40 +0000

    Fix provider

--- examples/eks-bootstrap-no-modules/providers.tf
+++ examples/eks-bootstrap-no-modules/providers.tf
@@ -4,10 +4,5 @@ provider "aws" {
 
 provider "kubernetes" {
   host = aws_eks_cluster.eks_cluster.cluster_endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command     = "aws"
-  }
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.cert)
 }
