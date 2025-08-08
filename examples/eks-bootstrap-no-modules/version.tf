ecc68c14028cfd0793b4f7f2125ecda821296f4a
Ali anjo <alianjo178@gmail.com>
2025-08-08 18:44:42 +0000

    Add: Move Statefile to S3

--- examples/eks-bootstrap-no-modules/version.tf
+++ examples/eks-bootstrap-no-modules/version.tf
@@ -1,9 +1,17 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-on-aws-eks-pro"
+    key    = "eks-cluster/terraform.tfstate"
+    region = "us-east-1" 
+ 
+    # For State Locking
+  }  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  
     kubernetes = {
       source = "hashicorp/kubernetes"
       version = ">= 2.20"
