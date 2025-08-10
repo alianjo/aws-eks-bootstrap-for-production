29206702b962738816e758823210f6ac9a29f8db
Ali anjo <alianjo178@gmail.com>
2025-08-10 19:53:00 +0000

    Fix syntax

--- examples/eks-bootstrap-no-modules/version.tf
+++ examples/eks-bootstrap-no-modules/version.tf
@@ -2,17 +2,17 @@ terraform {
   backend "s3" {
     bucket = "terraform-on-aws-eks-pro"
     key    = "eks-cluster/terraform.tfstate"
-    region = "us-east-1" 
+    region = "us-east-1"
   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-  
+
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = ">= 2.20"
-    }    
+    }
   }
 }
