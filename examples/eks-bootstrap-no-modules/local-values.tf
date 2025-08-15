c599f9a6b29717bc836e86d54c59aeef1a6a0740
Ali anjo <alianjo178@gmail.com>
2025-08-15 19:05:36 +0000

    Add: Admin User

--- examples/eks-bootstrap-no-modules/local-values.tf
+++ examples/eks-bootstrap-no-modules/local-values.tf
@@ -1,6 +1,7 @@
 locals {
   environment = var.environment
   name        = "${var.team}-${var.environment}-${var.cluster_name}-eks-cluster"
+  eks_user    = "${var.cluster_name}-admin"
   common_tags = {
     environment = local.environment
     name        = local.name
